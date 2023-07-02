1d45b9a6ab889a8d3583ba7b5f827174f2949e3b
syed <talentinfo9999@gmail.com>
2023-07-02 21:00:38 +0530

    salry added

--- 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/entity/Employee.java
+++ 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/entity/Employee.java
@@ -1,9 +1,13 @@
 package com.in28minutes.springboot.basics.springbootin10steps.entity;
 
+import java.math.BigDecimal;
+
 public class Employee {
 
 
     private int id;
     private String name;
     private String address;
+
+    private BigDecimal salary;
 }
